2a512f14ae19cfbbebc9ac16904118043eab1ecc
宋岩叡 <b06902106@ntu.edu.tw>
2020-06-12 13:01:39 +0800

    Add tutorial

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -1,13 +1,13 @@
 import React	from 'react';
 
 const abstractStyle = {
-	'marginTop': '5%',
-	'textAlign': 'center'
+    'margin': '3%',
+	'textAlign': 'left'
 };
 
 const memberStyle = {
-	'marginTop': '5%',
-	'textAlign': 'center',
+	'marginLeft': '10%',
+	'textAlign': 'left',
 	'whiteSpace': 'pre'
 }
 
@@ -15,12 +15,21 @@ export default function Tutorial() {
 	return (
 		<div>
 			<div style={abstractStyle}>
-				First, fuck yourself,
+                <h3>Speakers<br/><br/></h3>
+				<h4 style={memberStyle}>
+                    1. Sign up<br/><br/>
+                    2. Pass the event code to listeners<br/><br/>
+                    3. Start uploading files!
+                </h4>
 			</div>
-			<h2 style={abstractStyle}>and keep fucking yourself...</h2>
-			<div style={memberStyle}>
-                !!!!!
-			</div>
+            <div style={abstractStyle}>
+                <h3>Listeners<br/><br/></h3>
+                <h4 style={memberStyle}>
+			        1. Fill in the event code and press "Join Now"<br/><br/>
+                    2. "Clik to Update"<br/><br/>
+                    3. Start downloading files!
+                </h4>
+            </div>
 		</div>
 	)
 }
