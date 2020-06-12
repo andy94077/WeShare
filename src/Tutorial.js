71d743ad6758b93925c9a0027cacb83e87751527
宋岩叡 <b06902106@ntu.edu.tw>
2020-06-12 13:12:12 +0800

    Update tutorial

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -15,15 +15,16 @@ export default function Tutorial() {
 	return (
 		<div>
 			<div style={abstractStyle}>
-                <h3>Speakers<br/><br/></h3>
+                <h3>Instructors<br/><br/></h3>
 				<h4 style={memberStyle}>
                     1. Sign up<br/><br/>
                     2. Pass the event code to listeners<br/><br/>
-                    3. Start uploading files!
+                    3. Start uploading files!<br/><br/>
+                    4. Copy event token to sign in 
                 </h4>
 			</div>
             <div style={abstractStyle}>
-                <h3>Listeners<br/><br/></h3>
+                <h3>Audiences<br/><br/></h3>
                 <h4 style={memberStyle}>
 			        1. Fill in the event code and press "Join Now"<br/><br/>
                     2. "Clik to Update"<br/><br/>
