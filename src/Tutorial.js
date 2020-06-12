ed385ed316d1d6e68e81bf02ad3c71ed705565f1
宋岩叡 <b06902106@ntu.edu.tw>
2020-06-12 15:00:02 +0800

    Update tutorial

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -1,6 +1,11 @@
 import React	from 'react';
-
+const container = {
+    'width': '100vw',
+    'height': '100vh'
+}
 const abstractStyle = {
+    'width': '100vw',
+    'height': '5vh',
     'margin': '3%',
 	'textAlign': 'left'
 };
@@ -10,26 +15,29 @@ const memberStyle = {
 	'textAlign': 'left',
 	'whiteSpace': 'pre'
 }
-
 export default function Tutorial() {
 	return (
-		<div>
+		<div style={container}>
 			<div style={abstractStyle}>
-                <h3>Instructors<br/><br/></h3>
-				<h4 style={memberStyle}>
+                <h4>Instructors<br/><br/></h4>
+			</div>
+            <div style={memberStyle}>
+                <h5>
                     1. Sign up<br/><br/>
                     2. Pass the event code to listeners<br/><br/>
                     3. Start uploading files!<br/><br/>
                     4. Copy event token to sign in 
-                </h4>
-			</div>
+                </h5>
+            </div>
             <div style={abstractStyle}>
-                <h3>Audiences<br/><br/></h3>
-                <h4 style={memberStyle}>
+                <h4>Audiences<br/><br/></h4>
+            </div>
+            <div style={memberStyle}>
+                <h5>
 			        1. Fill in the event code and press "Join Now"<br/><br/>
                     2. "Clik to Update"<br/><br/>
                     3. Start downloading files!
-                </h4>
+                </h5>
             </div>
 		</div>
 	)
