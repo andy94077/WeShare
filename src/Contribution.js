216c4bbb46f016de0c348bfbf38dcf20b79b918a
hc07180011 <henrychao0718@gmail.com>
2020-06-16 16:42:43 +0800

    tutorial/contricution

--- src/Contribution.js
+++ src/Contribution.js
@@ -1,34 +1,30 @@
-import React	from 'react';
-
-const sloganStyle = {
-	'marginTop': '10%',
-	'textAlign': 'center'
-};
-const titleStyle = {
-	'marginTop': '40%',
-    'textAlign': 'center'
-}
-const memberStyle = {
-    'marginTop': '5%',
-	'textAlign': 'center',
-}
+import React from 'react';
+import { Row, Col } from 'reactstrap';
 
 export default function Contribution() {
 	return (
-		<div>
-			<div style={sloganStyle}>
-			    <h3>A chrome extension<br/>
-                    that makes sharing easier.</h3>
-			</div>
-			<h4 style={titleStyle}>Developers</h4>
-			<div style={memberStyle}>
-				<h7>b06902001 陳義榮<br/>
-				    b06902017 趙允祥<br/>
-				    b06902024 黃秉迦<br/>
-		    		b06902029 裴梧鈞<br/>
-			    	b06902057 薛佳哲<br/>
-				    b06902106 宋岩叡</h7>
-			</div>
+        <div style={{ width: "90%", float: "none", margin: "0 auto" }} >
+            <div style={{height: "5vh"}}></div>
+            <Row>
+                <Col><h3 style={{ textAlign: "center" }}>A chrome extension<br/>that makes sharing easier.</h3></Col>
+            </Row>
+            <div style={{height: "10vh"}}></div>
+            <Row>
+                <Col><h4 style={{ textAlign: "center" }}>Developers</h4></Col>
+            </Row>
+            <div style={{height: "5vh"}}></div>
+            <Row>
+                <Col>
+                    <p style={{ textAlign: "center" }}>
+                        b06902001 陳義榮<br/>
+                        b06902017 趙允祥<br/>
+                        b06902024 黃秉迦<br/>
+                        b06902029 裴梧鈞<br/>
+                        b06902057 薛佳哲<br/>
+                        b06902106 宋岩叡
+                    </p>
+                </Col>
+            </Row>
 		</div>
 	)
 }
--- src/Tutorial.js
+++ src/Tutorial.js
@@ -1,51 +1,42 @@
-import React	from 'react';
-const institleStyle = { //instrouctor title
-    'marginTop': '5%',
-    'marginLeft': '2%',
-	'textAlign': 'left'
-};
+import React from 'react';
+import { Row, Col } from 'reactstrap';
 
-const audtitleStyle = { //audiance title
-    'marginTop': '20%',
-    'marginLeft': '2%',
-	'textAlign': 'left'
-};
-
-const insmemberStyle = {//instructor member
-	'marginLeft': '7%',
-    'marginTop': '-5%',
-	'textAlign': 'left',
-}
-
-const audmemberStyle = {//audiance member
-	'marginLeft': '7%',
-    'marginTop': '-5%',
-	'textAlign': 'left',
-}
 export default function Tutorial() {
 	return (
-		<div>
-			<div style={institleStyle}>
-                <h4>Instructors<br/><br/></h4>
-			</div>
-            <div style={insmemberStyle}>
-                <h6>
-                    1. Sign up<br/><br/>
-                    2. Pass the event code to listeners<br/><br/>
-                    3. Copy the event token to sign in again at <br/>&ensp;&ensp;any time<br/><br/>
-                    4. Start uploading files!
-                </h6>
-            </div>
-            <div style={audtitleStyle}>
-                <h4>Audiences<br/><br/></h4>
-            </div>
-            <div style={audmemberStyle}>
-                <h6>
-			        1. Fill in the event code and click "Join Now"<br/><br/>
-                    2. "Clik to Update"<br/><br/>
-                    3. Start downloading files!
-                </h6>
-            </div>
-		</div>
+        <div style={{ width: "90%", float: "none", margin: "0 auto" }} >
+            <Col>
+                <div style={{height: "5vh"}}></div>
+                <Row>
+                    <h4>Instructors</h4>
+                </Row>
+                <div style={{height: "5vh"}}></div>
+                <Row>
+                    <h6>1. Sign up</h6>
+                </Row>
+                <Row>
+                    <h6>2. Pass the event code to listeners</h6>
+                </Row>
+                <Row>
+                    <h6>3. Copy the event token to sign in again</h6>
+                </Row>
+                <Row>
+                    <h6>4. Start uploading files!</h6>
+                </Row>
+                <div style={{height: "5vh"}}></div>
+                <Row>
+                    <h4>Audiences</h4>
+                </Row>
+                <div style={{height: "5vh"}}></div>
+                <Row>
+                    <h6>1. Fill in the event code, click "Join Now"</h6>
+                </Row>
+                <Row>
+                    <h6>2. "Clik to Update"</h6>
+                </Row>
+                <Row>
+                    <h6>3. Start downloading files!</h6>
+                </Row>
+            </Col>
+        </div>
 	)
 }
