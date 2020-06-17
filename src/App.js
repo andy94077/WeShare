5d11220ae9359dd300df10e35c3d889f7c4f4652
hc07180011 <henrychao0718@gmail.com>
2020-06-18 00:52:47 +0800

    tab

--- src/App.js
+++ src/App.js
@@ -22,72 +22,72 @@ function Copyright() {
 			{'Copyright © '}
 				2020 WeShare. All rights reserved.
 		</Typography>
-	);
+    )
 }
 
 class App extends React.Component {
 	constructor() {
 		super()
-        try { this.state = JSON.parse(localStorage.getItem('localState')) } catch {}
-        if (this.state === null) { this.state = ({ tabIndex: 'Welcome', codeErrorMes: '', navExpanded: false }) } 
+		try { this.state = JSON.parse(localStorage.getItem('localState')) } catch {}
+		if (this.state === null) { this.state = ({ tabIndex: 'Welcome', codeErrorMes: '', navExpanded: false }) } 
 	}
-    componentDidUpdate = (_, __) => {
-        localStorage.setItem('localState', JSON.stringify(this.state)) 
-    }
-    setNavExpanded = (status) => {
-        if (status === false) this.setState({ navExpanded: false})
-        else this.setState({ navExpanded: this.state.navExpanded ? false : true })
-    }
+	componentDidUpdate = (_, __) => {
+		localStorage.setItem('localState', JSON.stringify(this.state)) 
+	}
+	setNavExpanded = (status) => {
+		if (status === false) this.setState({ navExpanded: false})
+		else this.setState({ navExpanded: this.state.navExpanded ? false : true })
+	}
 	handleClick = (key) => {
-        this.setState({ tabIndex: key })
-        if (key !== 'Welcome' || this.state.navExpanded)
-            this.setNavExpanded(false)
-    }
-    handleEventCode = (code) => {
-        this.setState({ activateEventCode: code })
-    }
-    handleEventTitle = (title) => {
-        this.setState({ activateEventTitle: title })
-    }
-    handleEventToken = (token) => {
-        this.setState({ activateEventToken: token })
-    }
+		this.setState({ tabIndex: key })
+		if (key !== 'Welcome' || this.state.navExpanded)
+			this.setNavExpanded(false)
+	}
+	handleEventCode = (code) => {
+		this.setState({ activateEventCode: code })
+	}
+	handleEventTitle = (title) => {
+		this.setState({ activateEventTitle: title })
+	}
+	handleEventToken = (token) => {
+		this.setState({ activateEventToken: token })
+	}
 	render() {
-        
-        let currentPage
+		
+		let currentPage
 
-        switch (this.state.tabIndex) {
-            case 'Welcome':
-                currentPage = <Welcome parent={this} />
-                break
-            case 'Sign Up':
-                currentPage = <SignUp parent={this} />
-                break
-            case 'Sign In':
-                currentPage = <SignIn parent={this} />
-                break
-            case 'Tutorial':
-                currentPage = <Tutorial parent={this} />
-                break
-            case 'About Us':
-                currentPage = <Contribution parent={this} />
-                break
-            case 'Teacher':
-                currentPage = <Upload parent={this} />
-                break
-            case 'Student':
-                currentPage = <Download parent={this} />
-                break
-            default:
-                currentPage = <Loading />
-        }
+		switch (this.state.tabIndex) {
+			case 'Welcome':
+				currentPage = <Welcome parent={this} />
+				break
+			case 'Sign Up':
+				currentPage = <SignUp parent={this} />
+				break
+			case 'Sign In':
+				currentPage = <SignIn parent={this} />
+				break
+			case 'Tutorial':
+				currentPage = <Tutorial parent={this} />
+				break
+			case 'About Us':
+				currentPage = <Contribution parent={this} />
+				break
+			case 'Teacher':
+				currentPage = <Upload parent={this} />
+				break
+			case 'Student':
+				currentPage = <Download parent={this} />
+				break
+			default:
+				currentPage = <Loading />
+		}
 
 		return (
 		<div>
-            <Navbar bg='light' expand='lg' onToggle={() => this.setNavExpanded(true)} expanded={this.state.navExpanded} >
+			<Navbar bg='light' expand='lg' onToggle={() => this.setNavExpanded(true)} expanded={this.state.navExpanded} >
 				<Navbar.Brand style={{ cursor: 'pointer' }} onClick={() => this.handleClick('Welcome')}>
-                    <img src={logo} alt='logo' height='50px'></img>WeShare β
-                </Navbar.Brand>
+					<img src={logo} alt='logo' height='50px'></img>WeShare β
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls='basic-navbar-nav' />
 				<Navbar.Collapse id='basic-navbar-nav' >
 					<Nav className='mr-auto'>
@@ -98,7 +98,7 @@ class App extends React.Component {
 						<Nav.Link onClick={() => this.handleClick('Tutorial')}>Tutorial</Nav.Link>
 						<Nav.Link onClick={() => this.handleClick('About Us')}>About Us</Nav.Link>
 					</Nav>
-                    <EventCode parent={this} />
+					<EventCode parent={this} />
 				</Navbar.Collapse>
 			</Navbar>
 			{currentPage}
